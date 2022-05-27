fdc8dc5589fe918c5f1e5fb48f7cd4ad0204a299
saattrupdan <saattrupdan@gmail.com>
2022-05-27 11:17:40 +0100

    feat: Improve TFIDF model

--- src/tfidf.py
+++ src/tfidf.py
@@ -7,7 +7,7 @@ import joblib
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
-from sklearn.metrics import matthews_corrcoef
+from sklearn.metrics import matthews_corrcoef, f1_score
 
 
 def train_tfidf_model(output_path: Union[str, Path] = "models/tfidf_model.bin"):
@@ -23,15 +23,12 @@ def train_tfidf_model(output_path: Union[str, Path] = "models/tfidf_model.bin"):
     # Create the TF-IDF vectorizer
     vectorizer = TfidfVectorizer(
         analyzer="word",
-        ngram_range=(1, 3),
-        min_df=0.01,
-        max_df=0.9,
-        max_features=10000,
-        sublinear_tf=True,
+        max_features=10_000,
+        norm=None,
     )
 
     # Create the logistic regression model
-    model = LogisticRegression(max_iter=1000)
+    model = LogisticRegression(max_iter=10_000)
 
     # Create the pipeline
     pipeline = Pipeline([("vectorizer", vectorizer), ("model", model)])
@@ -41,16 +38,24 @@ def train_tfidf_model(output_path: Union[str, Path] = "models/tfidf_model.bin"):
     train_df = dataset_dict["train"].to_pandas()
     test_df = dataset_dict["test"].to_pandas()
 
+    # Convert labels to 0/1
+    train_df["label"] = train_df["label"].apply(lambda x: 1 if x == "OFF" else 0)
+    test_df["label"] = test_df["label"].apply(lambda x: 1 if x == "OFF" else 0)
+
     # Fit the vectorizer to the data and transform it
     pipeline.fit(train_df.text, train_df.label)
 
     # Score the model and output the results
     train_preds = pipeline.predict(train_df.text)
     test_preds = pipeline.predict(test_df.text)
-    train_score = matthews_corrcoef(train_df.label, train_preds)
-    test_score = matthews_corrcoef(test_df.label, test_preds)
-    print(f"Train score: {train_score}")
-    print(f"Test score: {test_score}")
+    train_mcc = matthews_corrcoef(train_df.label, train_preds)
+    train_f1 = f1_score(train_df.label, train_preds)
+    test_mcc = matthews_corrcoef(test_df.label, test_preds)
+    test_f1 = f1_score(test_df.label, test_preds)
+    print(f"Train MCC: {100 * train_mcc:.2f}%")
+    print(f'Train F1: {100 * train_f1:.2f}%')
+    print(f"Test MCC: {100* test_mcc:.2f}%")
+    print(f'Test F1: {100 * test_f1:.2f}%')
 
     # Save the pipeline
     output_path = Path(output_path)
