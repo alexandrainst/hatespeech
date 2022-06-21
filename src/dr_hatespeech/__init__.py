84a34add28ed5d3a5127358364165878c7e5c8c3
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2022-06-21 13:12:04 +0200

    chore: Move logging config to __init__

--- src/dr_hatespeech/__init__.py
+++ src/dr_hatespeech/__init__.py
@@ -1,1 +1,7 @@
 """Hatespeech detection for DR Facebook data."""
+
+import logging
+
+# Set up logging
+fmt = "%(asctime)s [%(levelname)s] <%(name)s> %(message)s"
+logging.basicConfig(level=logging.INFO, format=fmt)
