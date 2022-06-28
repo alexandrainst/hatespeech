4bcfe070cac9d13b5d809b1137d80b09ee113162
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2022-06-28 15:44:05 +0200

    tests: Add conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+"""Session-wide fixtures for tests."""
+
+import pytest
+from hydra import compose, initialize
+
+# Initialise Hydra
+initialize(config_path="../config", version_base=None)
+
+
+@pytest.fixture(scope="session")
+def config():
+    cfg = compose(config_name="config")
+    cfg.test = True
+    return cfg
