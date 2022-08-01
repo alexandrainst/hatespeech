89e30a0c37ab19940b9e014e134bf1445119e280
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2022-08-01 11:05:34 +0200

    style: Remove non-descriptive comments

--- tests/test_weak_supervision.py
+++ tests/test_weak_supervision.py
@@ -5,11 +5,6 @@ from src.dr_hatespeech.weak_supervision import apply_weak_supervision
 
 
 def test_weak_supervision(config):
-    # Apply weak supervision, generating the weakly supervised test dataset
     apply_weak_supervision(config)
-
-    # Load the weakly supervised test dataset
     df = load_weakly_supervised_data(config)
-
-    # Check that the weakly supervised test dataset has a "label" column
     assert "label" in df.columns
