b961c9fc7dd6eaac5f82bf3b4c5e295aa3529e52
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2022-08-01 11:03:11 +0200

    style: Remove unused imports

--- tests/test_weak_supervision.py
+++ tests/test_weak_supervision.py
@@ -1,11 +1,7 @@
 """Unit tests related to the weak_supervision module."""
 
-import pytest
-
 from src.dr_hatespeech.load_data import load_weakly_supervised_data
 from src.dr_hatespeech.weak_supervision import apply_weak_supervision
-
-ws = pytest.importorskip("src.dr_hatespeech.weak_supervision")
 
 
 def test_weak_supervision(config):
